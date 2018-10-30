88d616534c664cbcc6c3adc4b65f82888afce4f6
Nathan Isaac <nathanjisaac@gmail.com>
2018-10-29 22:35:07 -0700

    Add stats component templates

---
+++ src/components/stats/Stats.test.js
@@ -0,0 +1,9 @@
+// keeps track of how many times the quiz has been taken
+
+// keeps track of scores
+
+// keeps track of performance on a per-card basis
+
+it('do something', () => {
+  expect(true).toBe(true);
+}); 
